6c1dd0e4f2ff2654d7465fadc96d1118da82664f
Your Name <you@example.com>
2020-03-11 22:03:18 -0300

    Versao Final

--- Calculadora.java
+++ Calculadora.java
@@ -7,13 +7,14 @@ public class Calculadora {
 		String operador = JOptionPane.showInputDialog("Escolha a operacao desejada: "
 				+ "\n1 - Somar;"
 				+ "\n2 - Subtrair;"
-				+ "\n3 - Sair;");
+				+ "\n3 - Multiplicar;"
+				+ "\n4 - Dividir;"
+				+ "\n5 - Sair;");
 		
 		int operacao;
 		operacao = Integer.parseInt(operador);
 		
-		// 
-		if (operacao == 1) {
+			if (operacao == 1) {
 			
 			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
 			int n2 = Integer.parseInt( JOptionPane.showInputDialog("Digite o segundo numero: "));
@@ -34,6 +35,28 @@ public class Calculadora {
 			subtrair = n1 - n2;
 			
 			JOptionPane.showMessageDialog(null, "Resultado: " + subtrair);
+			
+		}
+		else if(operacao == 3) {
+			
+			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+			int n2 = Integer.parseInt( JOptionPane.showInputDialog("Digite o segundo numero: "));
+			
+			int multiplicar;
+			multiplicar = n1 * n2;
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + multiplicar);
+			
+		}
+		else if(operacao == 4) {
+			
+			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+			int n2 = Integer.parseInt( JOptionPane.showInputDialog("Digite o segundo numero: "));
+			
+			int dividir;
+			dividir = n1 / n2;
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + dividir);
 			
 		}
 		
